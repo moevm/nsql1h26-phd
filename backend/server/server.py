75eeb8781ea63a00eb7db70262d6b61300e175b0
Lvov Alexandr <strangeisdoctor@gmail.com>
2026-05-11 14:42:35 +0300

    start app

--- backend/server/server.py
+++ backend/server/server.py
@@ -4,8 +4,8 @@ from typing import Optional
 
 import uvicorn
 
-from database.arango import DatabaseManager
-from parser.parser import VakParser
+from backend.database.arango import DatabaseManager
+from backend.parser.parser import VakParser
 
 app = FastAPI(
     title="Dissertation API",
@@ -109,11 +109,3 @@ def get_organization_details(org_id: str):
     
     return result
 
-
-if __name__ == "__main__":
-    uvicorn.run(
-        "server.server:app",
-        host="0.0.0.0",
-        port=3000,
-        reload=True
-    )
