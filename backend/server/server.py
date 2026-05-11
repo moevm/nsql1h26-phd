c803acac15a79d393693dd8940c09dd74e61e37c
markluchkin <markluckin7@gmail.com>
2026-05-11 11:12:01 +0300

    fix imports

--- backend/server/server.py
+++ backend/server/server.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI, HTTPException, Query
+
 from arango import DatabaseManager
+from parser import VakParser
 
 app = FastAPI(
     title="",
     version="1.0.0"
 )
+
+db = DatabaseManager().connect()
+
