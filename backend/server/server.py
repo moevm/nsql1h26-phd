7b75f300999fc8af016078c7adee0e4888ea2c63
markluchkin <markluckin7@gmail.com>
2026-05-11 11:31:06 +0300

    add /api/dissertations endpoint

--- backend/server/server.py
+++ backend/server/server.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException, Query
+from fastapi.responses import Response
 
 from database.arango import DatabaseManager
 from parser.parser import VakParser
@@ -9,4 +10,16 @@ app = FastAPI(
 )
 
 db = DatabaseManager().connect()
+
+@app.get("/api/dissertations/{diss_id}")
+def get_dissertation_details(diss_id: str):
+    result = db.get_dissertation_details(diss_id)
+
+    if not result:
+        raise HTTPException(
+            status_code=404,
+            detail="dissertation not found"
+        )
+
+    return result
 
